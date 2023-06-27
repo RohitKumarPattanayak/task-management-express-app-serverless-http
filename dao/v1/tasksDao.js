78e0666694bdaf632da73e29a7bae862047a0c22
Rohit kumar pattanayak <rohit.p@spurtreetech.com>
2023-06-27 17:59:05 +0530

    revamped json data

--- dao/v1/tasksDao.js
+++ dao/v1/tasksDao.js
@@ -1,23 +1,24 @@
-import data from "../database/data.json" assert { type: "json" };
+import { createRequire } from "module";
 import _ from "lodash";
 import { CodeError } from "../../Handlers/ErrorHandler.js";
+const require = createRequire(import.meta.url);
 
+const data = require("../database/data.json");
 export const get_tasks_by_id = (id) => {
   return { data: data.filter((itr) => itr["id"] == id) };
 };
 
 export const get_tasks = () => {
-  let result = data;
-  return { data: result };
+  return { data: data.task };
 };
 
 export const post_new_task = (title, desc) => {
-  const duplicate = data.filter((itr) => itr["title"] == title);
+  const duplicate = data.task.filter((itr) => itr["title"] == title);
   if (_.size(duplicate)) {
     throw new CodeError({ message: "title should be unique." }, 403);
   }
-  data.push({
-    id: _.size(data) + 1,
+  data.task.push({
+    id: _.size(data.task) + 1,
     title,
     desc,
   });
@@ -25,9 +26,9 @@ export const post_new_task = (title, desc) => {
 };
 
 export const update_task_by_id = (id, title, desc) => {
-  const if_id_exists = data.filter((itr) => itr["id"] == id);
+  const if_id_exists = data.task.filter((itr) => itr["id"] == id);
   if (_.size(if_id_exists)) {
-    data.forEach((itr) => {
+    data.task.forEach((itr) => {
       if (itr["id"] == id) {
         itr["title"] = title;
         itr["desc"] = desc;
@@ -42,9 +43,9 @@ export const update_task_by_id = (id, title, desc) => {
 };
 
 export const patch_task_by_id = (id, body) => {
-  const if_id_exists = data.filter((itr) => itr["id"] == id);
+  const if_id_exists = data.task.filter((itr) => itr["id"] == id);
   if (_.size(if_id_exists)) {
-    data.forEach((itr) => {
+    data.task.forEach((itr) => {
       if (itr["id"] == id) {
         itr[Object.keys(body)[0]] = body[Object.keys(body)[0]];
       }
@@ -58,15 +59,15 @@ export const patch_task_by_id = (id, body) => {
 };
 
 export const delete_task_by_id = (id) => {
-  const if_id_exists = data.filter((itr) => itr["id"] == id);
+  const if_id_exists = data.task.filter((itr) => itr["id"] == id);
   if (_.size(if_id_exists)) {
     let delete_index;
-    data.map((itr, i) => {
+    data.task.map((itr, i) => {
       if (itr["id"] == id) {
         delete_index = i;
       }
     });
-    data.splice(delete_index, 1);
+    data.task.splice(delete_index, 1);
     return {
       Message: `Succesfully deleted the task with id : ${id}.`,
     };
